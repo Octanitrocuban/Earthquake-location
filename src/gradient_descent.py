d80920303c321ef3e69fc54cd9b393c11d71fb75
Octanitrocuban <113181068+Octanitrocuban@users.noreply.github.com>
2024-02-24 08:54:35 +0100

    with ensemble method
    Contain function to use ensemble method.

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -8,17 +8,38 @@ import numpy as np
 from scipy.spatial.distance import cdist
 from copy import deepcopy
 #=============================================================================
+def print_sample_array(sample, before='', after=''):
+	"""
+	Function to plot samples array as dictionary.
+
+	Parameters
+	----------
+	sample : numpy.ndarray
+		Sample that we want to print.
+	before : str, optional
+		Text to be put before the sample value's. The default is ''.
+	after : str, optional
+		Text to be put after the sample value's. The default is ''.
+
+	Returns
+	-------
+	None
+
+	"""
+	dico = {'X':sample[0], 'Y':sample[1], 'Z':sample[2], 't':sample[3]}
+	print(before+'{}'.format(dico)+after)
+
 def centrering(stations, key, event=None):
 	"""
 	Function to normalise (centralise) stations and event if given.
 
 	Parameters
 	----------
-	stations : dict
+	stations : dict or numpy.ndarray
 		Station locations and picked arrival time.
-	key : str
+	key : str or int
 		Item key to select the reference station.
-	event : dict
+	event : dict or numpy.ndarray
 		Event to be normalised with the key reference station. The default
 		is None.
 
@@ -30,22 +51,37 @@ def centrering(stations, key, event=None):
 		Normalised (centralised) event relative to the reference station.
 
 	"""
-	site_ref = deepcopy(stations[key])
 	if type(event) == dict:
+		site_ref = deepcopy(stations[key])
 		event['X'] = event['X']-site_ref['X']
 		event['Y'] = event['Y']-site_ref['Y']
 		event['Z'] = event['Z']-site_ref['Z']
 		event['t'] = event['t']-site_ref['t']
 
-	for i in stations:
-		stations[i]['X'] = stations[i]['X']-site_ref['X']
-		stations[i]['Y'] = stations[i]['Y']-site_ref['Y']
-		stations[i]['t'] = stations[i]['t']-site_ref['t']
+	elif type(event) == np.ndarray:
+		event = event-stations[key]
 
-	if type(event) == dict:
-		return stations, event
+	if type(stations) == dict:
+		site_ref = deepcopy(stations[key])
+		for i in stations:
+			stations[i]['X'] = stations[i]['X']-site_ref['X']
+			stations[i]['Y'] = stations[i]['Y']-site_ref['Y']
+			stations[i]['t'] = stations[i]['t']-site_ref['t']
+
+		if type(event) == dict:
+			return stations, event
+		else:
+			return stations
+
+	elif type(stations) == np.ndarray:
+		stations = stations-stations[key]
+		if type(event) == np.ndarray:
+			return stations, event
+		else:
+			return stations
+
 	else:
-		return stations
+		raise TypeError('stations must be dict or numpy.ndarray.')
 
 def calc_misfit(stations, event, vp=4000):
 	"""
@@ -80,8 +116,8 @@ def calc_misfit(stations, event, vp=4000):
 	misfit = m.sqrt(misfit_sum_sq) / len(stations)
 	return misfit
 
-def descente_gradient(stations, event_test, n_iteration, ranX=1, ranY=1,
-					  ranZ=1, rant=0.0005, l_r_m=0.2, patience=1000,
+def descente_gradient(stations, event_test, n_iteration, ran_x=1, ran_y=1,
+					  ran_z=1, ran_t=0.0005, l_r_m=0.2, patience=1000,
 					  vp=4000):
 	"""
 	Function to make a gradient descent from a given test event and data from
@@ -95,13 +131,13 @@ def descente_gradient(stations, event_test, n_iteration, ranX=1, ranY=1,
 		Event parameters : location and time {X, T, Z, t}.
 	n_iteration : int
 		Maximal number of iteration.
-	ranX : float, optional
+	ran_x : float, optional
 		Specified learning rate for the X parameter. The default is 1.
-	ranY : float, optional
+	ran_t : float, optional
 		Specified learning rate for the Y parameter. The default is 1.
-	ranZ : float, optional
+	ran_z : float, optional
 		Specified learning rate for the Z parameter. The default is 1.
-	rant : float, optional
+	ran_t : float, optional
 		Specified learning rate for the t parameter. The default is 0.0005.
 	l_r_m : float, optional
 		General learning rate. The default is 0.2.
@@ -119,16 +155,16 @@ def descente_gradient(stations, event_test, n_iteration, ranX=1, ranY=1,
 
 	"""
 	# Specified learning rate
-	rant = rant*l_r_m
-	ranX = ranX*l_r_m
-	ranY = ranY*l_r_m
-	ranZ = ranZ*l_r_m
+	ran_x_lr = ran_x*l_r_m
+	ran_y_lr = ran_y*l_r_m
+	ran_z_lr = ran_z*l_r_m
+	ran_t_lr = ran_t*l_r_m
 	p3p = int(patience*3)
 	p2p = int(patience*2)
 	# Gives us as an indication the misfit before applying gradient descent
 	misfit_ini = calc_misfit(stations, event_test, vp)
-	# print('Starting event = {}, starting misfit = {:.10f}'.format(
-	# 												event_test, misfit_ini))
+	print('Starting event = {}, starting misfit = {:.10f}'.format(
+													event_test, misfit_ini))
 
 	history = [[event_test['X'], event_test['Y'],
 				event_test['Z'], event_test['t']]]
@@ -136,44 +172,44 @@ def descente_gradient(stations, event_test, n_iteration, ranX=1, ranY=1,
 	cost_story = [calc_misfit(stations, event_test, vp)]
 	for i in range(n_iteration):
 		test = event_test.copy()
-		test['X'] += ranX
+		test['X'] += ran_x_lr
 		if (calc_misfit(stations, test, vp)-
 			calc_misfit(stations, event_test, vp)) < 0:
-			event_test['X'] += ranX
+			event_test['X'] += ran_x_lr
 
 		elif (calc_misfit(stations, test, vp)-
 				calc_misfit(stations, event_test, vp)) > 0:
-			event_test['X'] -= ranX
+			event_test['X'] -= ran_x_lr
 
 		test = event_test.copy()
-		test['Y'] += ranY
+		test['Y'] += ran_y_lr
 		if (calc_misfit(stations, test, vp)-
 			calc_misfit(stations, event_test, vp)) < 0:
-			event_test['Y'] += ranY
+			event_test['Y'] += ran_y_lr
 
 		elif (calc_misfit(stations, test, vp)-
 				calc_misfit(stations, event_test, vp)) > 0:
-			event_test['Y'] -= ranY
+			event_test['Y'] -= ran_y_lr
 
 		test = event_test.copy()
-		test['Z'] += ranZ
+		test['Z'] += ran_z_lr
 		if (calc_misfit(stations, test, vp)-
 			calc_misfit(stations, event_test, vp)) < 0:
-			event_test['Z'] += ranZ
+			event_test['Z'] += ran_z_lr
 
 		elif (calc_misfit(stations, test, vp)-
 				calc_misfit(stations, event_test, vp)) > 0:
-			event_test['Z'] -= ranZ
+			event_test['Z'] -= ran_z_lr
 
 		test = event_test.copy()
-		test['t'] += rant
+		test['t'] += ran_t_lr
 		if (calc_misfit(stations, test, vp)-
 			calc_misfit(stations, event_test, vp)) < 0:
-			event_test['t'] += rant
+			event_test['t'] += ran_t_lr
 
 		elif (calc_misfit(stations, test, vp)-
 				calc_misfit(stations, event_test, vp)) > 0:
-			event_test['t'] -= rant
+			event_test['t'] -= ran_t_lr
 
 		cost_story.append(calc_misfit(stations, event_test, vp))
 		history.append([event_test['X'], event_test['Y'], event_test['Z'],
@@ -191,7 +227,91 @@ def descente_gradient(stations, event_test, n_iteration, ranX=1, ranY=1,
 	cost_story = np.array(cost_story)
 	history = np.array(history, dtype=object)
 	misfit_fin = calc_misfit(stations, event_test, vp)
-	# print("Best event = {}, best misfit = {:.10f}".format(event_test,
-	# 														misfit_fin))
+	print("Best event = {}, best misfit = {:.10f}".format(event_test,
+															misfit_fin))
 
 	return event_test, cost_story, history
+
+
+def ensemble_descent(stations, n_samples, learning_rates, n_it, v_propag,
+					 limits, patience=1000):
+	"""
+	Function to make a gradient descent with many samples in a vectorised
+	method.
+
+	Parameters
+	----------
+	stations : numpy.ndarray
+		Station locations and picked arrival time.
+	n_samples : int
+		Number of sample with wich the gradient descent will be done.
+	learning_rates : numpy.ndarray
+		Learning rates of the parameters. If a rate is at 0 the parameter
+		will not be updated.
+	n_it : int
+		Maximal number of iteration.
+	v_propag : float
+		Assumed constant P-wave velocity.
+	limits : list
+		List of list of float. Size is (n, 2). 'n' is the number of features.
+		The first n-th value is the lower bound and the second is the upper
+		bound of the random search.
+	patience : int, optional
+		Patience for the early stoping. The default is 1000.
+
+	Returns
+	-------
+	loss : numpy.ndarray
+		Loss history at each iteration.
+	history : numpy.ndarray
+		Listing of the best event at each iteration.
+
+	"""
+	p2p = int(patience*2)
+	nxis = np.newaxis
+	samps_pk = np.arange(n_samples)*16
+	sample = np.array([
+		np.random.uniform(limits[0][0], limits[0][1], n_samples),
+		np.random.uniform(limits[1][0], limits[1][1], n_samples),
+		np.random.uniform(limits[2][0], limits[2][1], n_samples),
+		np.zeros(n_samples)-1]).T
+
+	kernel = np.array([[ 1,  1,  1,  1], [-1,  1,  1,  1], [ 1, -1,  1,  1],
+					   [ 1,  1, -1,  1], [ 1,  1,  1, -1], [-1, -1,  1,  1],
+					   [-1,  1, -1,  1], [-1,  1,  1, -1], [ 1, -1, -1,  1],
+					   [ 1, -1,  1, -1], [ 1,  1, -1, -1], [-1, -1, -1,  1],
+					   [-1, -1,  1, -1], [-1,  1, -1, -1], [ 1, -1, -1, -1],
+					   [-1, -1, -1, -1]])*learning_rates
+
+	dist_v = cdist(stations[:, :3], sample[:, :3]).T
+	pred_t = sample[:, 3:4] + dist_v / v_propag
+	rmse = np.sum((pred_t-stations[:, 3])**2, axis=1)**.5 /len(stations)
+	print('Starting misfit = '+str(np.mean(rmse)))
+	loss = []
+	history = []
+	for i in range(n_it):
+		# reaserch shape = (n samples*16, 4)
+		reaserch = np.reshape(sample[:, nxis]+kernel, (n_samples*16, 4))
+		# dist_v shape = (n samples, 16, n stations)
+		dist_v = cdist(stations[:, :3], reaserch[:, :3]).T
+		# pred_t shape = (n samples*16, n stations)
+		pred_t = reaserch[:, 3:4] + dist_v / v_propag
+		# rmse shape = (n samples*16)
+		rmse = np.sum((pred_t-stations[:, 3])**2, axis=1)**.5 /len(stations)
+		best = np.argmin(np.reshape(rmse, (n_samples, 16)), axis=1)+samps_pk
+		loss.append(rmse[best])
+		sample = reaserch[best]
+		history.append(sample)
+		if (i > p2p)&((i%patience) == 0):
+			sub_loss = np.array(loss[-p2p:])
+			pre_1 = np.mean(sub_loss[:patience], axis=0)
+			pre_2 = np.mean(sub_loss[patience:], axis=0)
+			if np.sum(pre_1 <= pre_2) == n_samples:
+				break
+
+	loss = np.array(loss)
+	print("Best misfit = "+str(np.min(loss[-1])))
+	best = np.argwhere(loss == np.min(loss))[-1]
+	history = np.array(history)
+	print_sample_array(history[best[0], best[1]], before='Best event = ')
+	return loss, history
